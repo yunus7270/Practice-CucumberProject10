f3fad51475f17bd731dc719d3c9a1f7a2df82665
yunus <yunus7270@gmail.com>
2021-01-26 09:09:30 -0600

    Yunus added new lines

--- src/main/java/yunuso/CucumberProject10/App.java
+++ src/main/java/yunuso/CucumberProject10/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Merhabe Dunya!" );
+        System.out.println( "Merhaba Dunya!" );
+        System.out.println( "Selam Dunya!" );
     }
 }
